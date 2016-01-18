9de0626900cec7a5e3a04fd975eb9901489132c7
Mario Alvarez <mario@mobillogix.com>
2016-01-18 07:34:34 -0500

    Dynamic components

--- src/front/module.js
+++ src/front/module.js
@@ -1,7 +1,7 @@
 export class FrontModule {
   configureRouter(config, router) {
     config.map([
-      { route: '', name: 'front-home', moduleId: './views/home', nav: true, title: 'Home' },
+      { route: '', name: 'front-home', moduleId: './views/home/home', nav: true, title: 'Home' },
       { route: 'products', name: 'front-products', moduleId: './views/products/module', nav: true, title: 'Products' },
       { route: 'account', name: 'front-account', moduleId: './views/account/module', nav: true, title: 'Account' }
     ]);
--- src/front/views/home/home.js
+++ src/front/views/home/home.js
@@ -3,16 +3,21 @@ import {HttpClient} from 'aurelia-fetch-client';
 
 @inject(HttpClient)
 export class Home {
+  overridableSlider = 'default';
+  sliderOptions = ['default', 'gcn', 'adcom'];
+
   constructor(http) {
     this.http = http;
+  }
+
+  switchSlider(e) {
+    this.overridableSlider = e.target.value;
   }
 
   activate() {
     return this.http.fetch('info')
       .then(response => response.json())
-      .then(response => {
-        console.log(response);
-      })
+      .then(response => {})
       .catch(() => {});
   }
 }
